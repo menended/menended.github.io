41aaf228b99b567d5c4be45adafebc9e12f627ef
menended <menended@byui.edu>
2021-04-27 00:22:23 -0600

    Second update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let d = new Date();
 let theyear = d.getFullYear();
-document.getElementById("currentyear").textContent = theyear;
+document.querySelector("#currentyear").textContent = theyear;
 
 document.querySelector('#lastupdate').textContent = document.lastModified;
