a009361eaf5970a6961e0d9769119a64f4cbe799
menended <menended@byui.edu>
2021-05-15 16:38:28 -0600

    1st update

---
+++ lesson4/js/index.js
@@ -0,0 +1,5 @@
+let d = new Date();
+let theyear = d.getFullYear();
+document.querySelector("#currentyear").textContent = theyear;
+
+document.querySelector('#lastupdate').textContent = document.lastModified;
