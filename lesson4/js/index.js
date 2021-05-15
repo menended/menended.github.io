2bcc07c67e26b15d48dff852551219d6b82938db
menended <menended@byui.edu>
2021-05-15 17:36:08 -0600

    Final Update

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -1,5 +1,14 @@
+/*Information - Footer*/
 let d = new Date();
 let theyear = d.getFullYear();
 document.querySelector("#currentyear").textContent = theyear;
-
 document.querySelector('#lastupdate').textContent = document.lastModified;
+
+/*Responsive Navigation Bar/*
+/*const button = document.querySelector(".ham-menu");
+const navigation = document.querySelector(".nav-bar");
+button.addEventListener("click", () => {navigation.classList.toggle("responsive")}, false);*/
+
+function responsiveNav() {
+    document.getElementsByClassName("nav-bar")[0].classList.toggle("responsive");
+}
