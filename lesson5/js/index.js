5f8579213235a576c66c794830bf2e41d3e3a44a
menended <menended@byui.edu>
2021-05-24 23:06:13 -0600

    Lesson 5 - Update

---
+++ lesson5/js/index.js
@@ -0,0 +1,16 @@
+// Current Day
+const datefield = document.querySelector("date");
+const now = new Date ();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.textContent = fulldate;
+
+// Last Updated
+let d = new Date();
+let theyear = d.getFullYear();
+document.querySelector("#currentyear").textContent = theyear;
+document.querySelector('#lastupdate').textContent = document.lastModified;
+
+//Responsive Navigation Bar
+function responsiveNav() {
+    document.getElementsByClassName("nav-bar")[0].classList.toggle("responsive");
+}
