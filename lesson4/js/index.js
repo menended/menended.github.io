e960bed2b8143e8156e789abb9d3f312a69c11d2
menended <menended@byui.edu>
2021-05-24 20:42:35 -0600

    Week 4 - Updated

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -1,14 +1,16 @@
-/*Information - Footer*/
+// Current Day
+const datefield = document.querySelector("date");
+const now = new Date ();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+datefield.textContent = fulldate;
+
+// Last Updated
 let d = new Date();
 let theyear = d.getFullYear();
 document.querySelector("#currentyear").textContent = theyear;
 document.querySelector('#lastupdate').textContent = document.lastModified;
 
-/*Responsive Navigation Bar/*
-/*const button = document.querySelector(".ham-menu");
-const navigation = document.querySelector(".nav-bar");
-button.addEventListener("click", () => {navigation.classList.toggle("responsive")}, false);*/
-
+//Responsive Navigation Bar
 function responsiveNav() {
     document.getElementsByClassName("nav-bar")[0].classList.toggle("responsive");
 }
