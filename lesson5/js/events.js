12f263d03169cef791db6096149a915e2f1e10c2
menended <menended@byui.edu>
2021-05-26 01:56:48 -0600

    Lesson 5 - Small View

---
+++ lesson5/js/events.js
@@ -0,0 +1,5 @@
+//Banner
+let activities = new Date();
+if (activities.getDay() == 3) {
+    document.querySelector('#banner').style.display = 'block';
+}
--- lesson5/js/index.js
+++ lesson5/js/index.js
@@ -1,10 +1,10 @@
-// Current Day
+//Current Day
 const datefield = document.querySelector("date");
 const now = new Date ();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 datefield.textContent = fulldate;
 
-// Last Updated
+//Last Updated
 let d = new Date();
 let theyear = d.getFullYear();
 document.querySelector("#currentyear").textContent = theyear;
